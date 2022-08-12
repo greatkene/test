e43247c85d8fa9a9d0d1b8cf40ecc71d77acb0f4
greatkene <ihevuemeg@gmail.com>
2022-08-12 17:28:22 +0100

    error fixed..

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DashboardLayout from "../layouts/dashboardLayout";
+import DashboardLayout from "../layouts/DashboardLayout";
 import { Text, Box, Flex } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
